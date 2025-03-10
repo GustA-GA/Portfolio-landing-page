b7839be69febc66457c1f5448aed767733975324
GustA-GA <g.alvarezftw@gmail.com>
2025-03-10 11:13:21 -0300

    Added projects hovering effect, as well as a couple fixes in order to increase UX

--- main.js
+++ main.js
@@ -3,6 +3,36 @@ document.addEventListener('DOMContentLoaded', () => {
     const techIconSect = document.querySelector('.tech-icons');
     const techIcons = techIconSect.querySelectorAll('img');
   
+    document.addEventListener("mousemove", (e) => {
+      const x = e.clientX; // Get cursor's X position
+      const y = e.clientY; // Get cursor's Y position
+  
+      // Update the background with a radial gradient centered at the cursor's position
+      document.body.style.background = `
+          radial-gradient(
+              circle 150px at ${x}px ${y}px,#0F0F7C,#0D0C52
+          )
+      `;
+    });
+
+
+    // Reading indicator
+    document.addEventListener('scroll', () => {
+      const sections = document.querySelectorAll('.main-column section');
+      const links = document.querySelectorAll('.sticky-info nav a');
+    
+      sections.forEach((section, index) => {
+        const rect = section.getBoundingClientRect();
+    
+        // Check if the section is in the viewport
+        if (rect.top <= 100 && rect.bottom >= 10) { // Adjust 100px offset if needed
+          links.forEach(link => link.classList.remove('active')); // Remove active class from all links
+          links[index].classList.add('active'); // Add active class to the corresponding link
+        }
+      });
+    });
+
+    // Opacity control for the contentBoxes
     contentBoxes.forEach(box => {
       box.addEventListener('mouseenter', () => {
         
@@ -20,6 +50,7 @@ document.addEventListener('DOMContentLoaded', () => {
       });
     });
 
+    // Opacity control for the icons
     techIcons.forEach(box => {
       box.addEventListener('mouseenter', () => {
 
